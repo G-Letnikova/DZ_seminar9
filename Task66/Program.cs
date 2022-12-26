3420e30b34a805347693098bdf8a67d64bac44e0
GLetnikova <g.letnikova@gmail.com>
2022-12-26 14:13:57 +0300

    DZ_sem9

---
+++ Task66/Program.cs
@@ -0,0 +1,23 @@
+﻿// 66 _ ввети M и N, найти сумму чисел между M и N ___________
+
+int sum(int a, int b)
+{
+    if (a == b)
+        return b;
+    if (a > b)
+        return 0;
+    return a + b + sum(a + 1, b - 1);
+}
+
+Console.Clear();
+Console.Write("Введите числа M и N: ");
+int[] number = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (number[0] >= number[1])
+{
+    Console.WriteLine("Первое число должно быть меньше второго");
+    Console.Write("Введите числа M и N: ");
+    number = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int m = number[0], n = number[1];
+Console.WriteLine(sum(m, n));
+
---
+++ Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// 68 _ вычисление функции Аккермана  ___________
+
+int FunkAkkerman(int a, int b)
+{
+    if (a == 0)
+        return b + 1;
+    if (a > 0 && b == 0)
+        return FunkAkkerman(a - 1, 1);
+    return FunkAkkerman(a - 1, FunkAkkerman(a, b - 1));
+
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FunkAkkerman(m, n));
+
